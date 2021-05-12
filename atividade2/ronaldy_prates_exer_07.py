deffb4eddeb67b640e6ec21bc6600ed32104500f
ro-prates <ronaldy1995@hotmail.com>
2021-05-11 22:21:13 -0300

    exercício 7 finalizado

---
+++ atividade2/ronaldy_prates_exer_07.py
@@ -0,0 +1,39 @@
+arquivo = open('seq_a.fasta', 'r')
+linhas = arquivo.readlines()
+arquivo.close()
+
+sequenciaA = []
+
+for linha in linhas:
+    if linha.find('>') != 0: # vai ignorar as linhas que não são importantes
+        sequenciaA.append(linha)
+
+arquivo = open('seq_b.fasta', 'r')
+linhas = arquivo.readlines()
+arquivo.close()
+
+sequenciaB = []
+
+for linha in linhas:
+    if linha.find('>') != 0: # vai ignorar as linhas que não são importantes
+        sequenciaB.append(linha)
+
+# for i in range(5):
+#     primeiro = sequenciaA[i]
+#     segundo = sequenciaB[i]
+#     for a in range(60):
+#         if primeiro[a] != segundo[a]:
+#             
+contador = 0
+trocas = 0
+
+for linha in range(5):
+    for coluna in range(60):
+        contador += 1
+        if contador > 200:
+            break
+        if sequenciaA[linha][coluna] != sequenciaB[linha][coluna]:
+            trocas += 1
+            print(f'a posição {contador} foi trocado {sequenciaA[linha][coluna]} --> {sequenciaB[linha][coluna]}')
+
+print(f'o número de nucleotídeos diferente é {trocas}')
