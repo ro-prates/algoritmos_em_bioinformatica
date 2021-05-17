805add29fb6d70e78f2acd1edcd17fff91ce162b
ro-prates <ronaldy1995@hotmail.com>
2021-05-16 22:04:20 -0300

    corrigindo exercício

--- atividade2/ronaldy_prates_exer_02.py
+++ atividade2/ronaldy_prates_exer_02.py
@@ -1,9 +1,10 @@
 deposito_inicial = float(input('digite o valor do deposito inicial:\n'))
 taxa_juros = float(input('digite o valor da taxa de juros:\n'))
-controle = deposito_inicial
+valor_juros = deposito_inicial * taxa_juros / 100
+controle_valor = deposito_inicial
 
 for i in range(24):
-    deposito_inicial += deposito_inicial * taxa_juros / 100
+    deposito_inicial += valor_juros
     print(f'Mês {1+i}: R${deposito_inicial:.2f}')
 
-print(f'\ntotal ganho: R${deposito_inicial-controle:.2f}')
+print(f'\ntotal ganho: R${deposito_inicial - controle_valor:.2f}')
